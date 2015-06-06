560bc62edd0853abde9357d6dd48ba7c80bb7c25
morvik <morvik@fake.com>
2015-06-06 12:16:36 +0600

    test

---
+++ csharp/TestingDojo2015/Tests/Release1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingDojo2015.Tests
+{
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+
+    public class Release1:BaseTestFixture
+    {
+        [Test]
+        public void SearchById()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("1");
+
+            var searchButton = mainWindow.FindElement(By.Id("SearchMW"));
+            searchButton.Click();
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            Assert.That(productItems.Count, Is.EqualTo(1));
+            var item = productItems.ElementAt(0);
+            Assert.AreEqual("Монитор Sony", item.Text);
+            
+
+        }
+
+
+        [Test]
+        public void SearchByFirstWord()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("Монитор");
+             var searchButton = mainWindow.FindElement(By.Id("SearchMW"));
+            searchButton.Click();
+          
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            Assert.That(productItems.Count, Is.EqualTo(1));
+            var item = productItems.ElementAt(0);
+            Assert.AreEqual("Монитор Sony", item.Text);
+        }
+
+
+        [Test]
+        public void SearchByLastWord()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("Microsoft");
+            var searchButton = mainWindow.FindElement(By.Id("SearchMW"));
+            searchButton.Click();
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            var item = productItems.ElementAt(0);
+            
+            Assert.AreEqual(item.Text,"Клавиатура Microsoft");
+
+        }
+
+
+        [Test]
+        public void SearchBySpace()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys(" ");
+            var searchButton = mainWindow.FindElement(By.Id("SearchMW"));
+            searchButton.Click();
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            
+
+           Assert.That(productItems.Count()==8);
+
+        }
+
+
+        [Test]
+        public void SearchByEnter()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+            var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+            searchString.SendKeys("1");
+
+            mainWindow.SendKeys(Keys.Enter);
+
+            var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+            var productItems = productsList.FindElements(By.ClassName("ListViewItem"));
+            Assert.That(productItems.Count, Is.EqualTo(1));
+            var item = productItems.ElementAt(0);
+            Assert.AreEqual("Монитор Sony", item.Text);
+        }
+
+
+
+    }
+}
---
+++ csharp/TestingDojo2015/Tests/Release2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingDojo2015.Tests
+{
+    using NUnit.Framework;
+
+    using OpenQA.Selenium;
+
+    public class Release2:BaseTestFixture
+    {
+        [Test]
+        public void IsModalDiaplays()
+        {
+            var mainWindow = this.Driver.FindElementById("MainWindow");
+
+           // var searchString = mainWindow.FindElement(By.Id("QueryMW"));
+             var productsList = mainWindow.FindElement(By.Id("ProductsMW"));
+             var firstList = productsList.FindElements(By.ClassName("ListViewItem"));
+            var addnew = mainWindow.FindElement(By.Id("AddNewProductMW"));
+            addnew.Click();
+            var tWindow = this.Driver.FindElementById("AddNewProductWindow");
+            var nameAw = tWindow.FindElement(By.Id("NameAW"));
+            nameAw.SendKeys("test");
+
+
+
+            
+
+
+        }
+    }
+}
